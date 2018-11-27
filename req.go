0bd7e7777a41f64710d73570f1b7455dc952febe
shijiada <jiada8866@126.com>
2018-11-27 17:11:50 +0800

    feat(trace): chang operation name
    rawurl in operation name may cause too many operation in Jaeger UI

--- req.go
+++ req.go
@@ -185,7 +185,7 @@ func (r *Req) Do(ctx context.Context, method, rawurl string, vs ...interface{}) 
 		ProtoMinor: 1,
 	}
 	req = req.WithContext(ctx)
-	req, ht := nethttp.TraceRequest(opentracing.GlobalTracer(), req, nethttp.OperationName(fmt.Sprintf("HTTP %s: %s", req.Method, rawurl)))
+	req, ht := nethttp.TraceRequest(opentracing.GlobalTracer(), req, nethttp.OperationName("req"))
 	defer ht.Finish()
 
 	resp = &Resp{req: req, r: r}
